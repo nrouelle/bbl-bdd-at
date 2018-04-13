1513185925f544b324add27f7f27f31b0d1c6c00
Nadege <nadege@rouelle.net>
2018-04-13 13:50:03 +0200

    [TU][Livraison] Ajout du premier test Livraison.SetPrix

--- Dojo.BDD.Test/LivraisonShould.cs
+++ Dojo.BDD.Test/LivraisonShould.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using Dojo.BDD.Exceptions;
-using Moq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Dojo.BDD.Test
 {
     [TestFixture]
     public class LivraisonShould
     {
+        [TestFixture]
+        public class SetPrixShould : LivraisonShould
+        {
+            [Test]
+            public void DoesNotThrowWhenSetPrixLivraisonPourUnPays()
+            {
+                var livraison = new Livraison();
+                Assert.DoesNotThrow(() => livraison.SetPrix("France", 10));
+            }
 
+            [Test]
+            public void SetPrixLivraisonPourUnPays()
+            {
+                var livraison = new Livraison();
+                Assert.DoesNotThrow(() => livraison.SetPrix("France", 10));
+            }
+        }
     }
 }
